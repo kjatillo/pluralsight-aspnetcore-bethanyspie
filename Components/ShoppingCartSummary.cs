67129bf6002af2864528f0b7eb8a094033b20e03
Ken <keneithatillo@gmail.com>
2024-05-29 17:09:49 +0100

    created view components + tag helpers, shop dropdown needs fix

---
+++ Components/ShoppingCartSummary.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pluralsight.AspNetCore.BethanysPie.Models;
+using Pluralsight.AspNetCore.BethanysPie.ViewModels;
+
+namespace Pluralsight.AspNetCore.BethanysPie.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly IShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(IShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel(_shoppingCart, _shoppingCart.GetShoppingCartTotal());
+
+            return View(shoppingCartViewModel);
+        }
+    }
+}
---
+++ Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pluralsight.AspNetCore.BethanysPie.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
