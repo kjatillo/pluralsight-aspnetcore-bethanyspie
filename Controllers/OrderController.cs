0ede49e978336e4b4ec273c5a2e815dbebad178a
kjatillo <x00190944@mytudublin.ie>
2024-06-02 15:15:43 +0100

    posted data accessed through model binding

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 using Pluralsight.AspNetCore.BethanysPie.Models;
 
 namespace Pluralsight.AspNetCore.BethanysPie.Controllers
@@ -16,6 +17,34 @@ namespace Pluralsight.AspNetCore.BethanysPie.Controllers
 
         public IActionResult Checkout()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout (Order order)
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Your cart is empty, add some pies first.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+                
+                return RedirectToAction("CheckoutComplete");
+            }
+
+            return View(order);
+        }
+
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Thanks for your order. You'll soon enjoy our delicious pies.";
             return View();
         }
     }
